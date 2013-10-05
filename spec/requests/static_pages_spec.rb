0b1bfa06470da37c0fd82cf268f4152fc1886d96
George Alonge <georgedelson@gmail.com>
2013-10-05 06:45:47 -0700

    Finish Static Pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the content 'Micro Feel'" do
+  		visit '/static_pages/home'
+      expect(page).to have_content('Micro Feel')
+    end
+
+    it "should have the title 'Home'" do
+    	visit '/static_pages/home'
+    	expect(page).to have_title("Micro Feel | Home")
+    end
+  end
+
+  describe "Help Page" do
+  	it "should have the content 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+    	visit '/static_pages/help'
+    	expect(page).to have_title("Micro Feel | Help")
+    end
+  end
+
+  describe "About Page" do
+  	it "should have the content 'About'" do
+  		visit '/static_pages/about'
+  		expect(page).to have_content('About')
+  	end
+
+  	it "should have the title 'About'" do
+    	visit '/static_pages/about'
+    	expect(page).to have_title("Micro Feel | About")
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+
+  #including the capybara DSL to the rspec helper file
+  config.include Capybara::DSL
 end
