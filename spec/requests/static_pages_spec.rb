7f082e4bbb4661dbcc4e529afcf0c36bdb97e888
George Alonge <georgedelson@gmail.com>
2013-10-06 18:52:12 -0700

    Update static page title to dynamic

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,10 +10,15 @@ describe "Static pages" do
       expect(page).to have_content('Micro Feel')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
     	visit '/static_pages/home'
-    	expect(page).to have_title("#{base_title} | Home")
+    	expect(page).to have_title("#{base_title}")
     end
+
+    it "should not have a title 'home'"
+    	visit '/static_pages/home'
+    	expect(page).not_to have_title('| Home')
+  	end
   end
 
   describe "Help Page" do
